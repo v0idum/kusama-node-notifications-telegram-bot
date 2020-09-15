a2cc7b8e77b9bc46b9b8f296312f121568ed2655
v0idum <akmal.melibaev@gmail.com>
2020-09-15 16:05:40 +0500

    format_balance has been added

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
 
 def trim_address(address):
     return address[:4] + '...' + address[-4:]
+
+
+def format_balance(number: str, digits_after_point=0):
+    res = float(number) / 1e12
+    return f'{res:,.{digits_after_point}f}'
