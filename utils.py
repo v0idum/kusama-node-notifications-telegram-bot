30be5e15b43f6ac0aecd182f9d8a8d698e090f8d
v0idum <akmal.melibaev@gmail.com>
2020-09-16 21:06:12 +0500

    get_index() added, returns character index of given occurrence

--- utils.py
+++ utils.py
@@ -6,3 +6,10 @@ def trim_address(address):
 def format_balance(number: str, digits_after_point=0):
     res = float(number) / 1e12
     return f'{res:,.{digits_after_point}f}'
+
+
+def get_index(input_string, char, ordinal):
+    current = -1
+    for i in range(ordinal):
+        current = input_string.index(char, current + 1)
+    return current
