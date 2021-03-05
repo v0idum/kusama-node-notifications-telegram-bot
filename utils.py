95a8ea2f06213f75b68975e01ce3acc318a4f602
Akmal Melibaev <akmal.melibaev@gmail.com>
2021-03-05 16:40:03 +0500

    balance formatting fix

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ def trim_address(address):
 
 
 def format_balance(number: str, digits_after_point=0):
-    res = float(number) / 1e12
+    res = float(number) / 1e10
     return f'{res:,.{digits_after_point}f}'
 
 
